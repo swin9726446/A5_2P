86b11e8357416d2771b05dcfbd1111ee1768585a
9726446 <9726446@student.swin.edu.au>
2018-09-19 17:47:14 +1000

    Work @ LB1-MAC-022 + Added Images Sourced from Wikimedia, attribution in Strings.xml

---
+++ app/src/main/java/com/example/a9726446/a4_2p/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a9726446.a4_2p;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

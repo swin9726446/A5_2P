30330ba79bbb093336f795312196d2662eea516b
swin9726446 <9726446@student.swin.edu.au>
2018-10-05 19:11:49 +1000

    App Completed @ LB1-MAC-029 + Finished app, now works like a charm + Fixed silly bug about null pointers // Still to be done: Reports - that's slated for Monday.

--- app/src/main/java/com/example/a9726446/a5_2p/MainActivity.java
+++ app/src/main/java/com/example/a9726446/a5_2p/MainActivity.java
@@ -1,136 +1,249 @@
 package com.example.a9726446.a5_2p;
-//
-//import android.content.Intent;
-//import android.support.v7.app.AppCompatActivity;
-//import android.os.Bundle;
-//import android.util.Log;
-//import android.view.View;
-//
-//public class MainActivity extends AppCompatActivity {
-//
-//    /**
-//     * Click Listeners for each button.
-//     * The char they pass is a key to tell which items viewFood needs to send.
-//     */
-//    private final View.OnClickListener anzacListener = new View.OnClickListener() {
-//        @Override
-//        public void onClick(View v) {
-//            viewFood('a');
-//        }
-//    };
-//    private final View.OnClickListener bologneseListener = new View.OnClickListener() {
-//        @Override
-//        public void onClick(View v) {
-//            viewFood('b');
-//        }
-//    };
-//    private final View.OnClickListener croissantListener = new View.OnClickListener() {
-//        @Override
-//        public void onClick(View v) {
-//            viewFood('c');
-//        }
-//    };
-//    private final View.OnClickListener pizzaListener = new View.OnClickListener() {
-//        @Override
-//        public void onClick(View v) {
-//            viewFood('p');
-//        }
-//    };
-//
-//
-//    /**
-//     * Activity for launching the bigger food view activity.
-//     * Switch statements are more fun to than nested if statements. (Subjective)
-//     * @param item a char key used to identify which food to examine.
-//     */
-//    private void viewFood(char item) {
-//        Intent i = new Intent();
-//        i.setClass(getApplicationContext(), ViewActivity.class);
-//        Bundle b = new Bundle();
-//        switch (item){
-//            case 'a':
-//                b.putInt("imageID", R.drawable.anzac_320px);
-//                b.putInt("captionID", R.string.strAnzac);
-//                b.putInt("srcID", R.string.strSrcAnzac);
-//                break;
-//            case 'b':
-//                b.putInt("imageID", R.drawable.bolognese_320px);
-//                b.putInt("captionID", R.string.strBolognese);
-//                b.putInt("srcID", R.string.strSrcBolognese);
-//                break;
-//            case 'c':
-//                b.putInt("imageID", R.drawable.croissant_320px);
-//                b.putInt("captionID", R.string.strCroissant);
-//                b.putInt("srcID", R.string.strSrcCroissant);
-//                break;
-//            case 'p':
-//                b.putInt("imageID", R.drawable.pizza_320px);
-//                b.putInt("captionID", R.string.strPizza);
-//                b.putInt("srcID", R.string.strSrcPizza);
-//                break;
-//            default:
-//                //Stops the launch of the next activity if switch fails.
-//                Log.e("Activity Launch", "Could not launch subsequent activity - Switch defaulted");
-//                return;
-//        }
-//        i.putExtra("bundle", b);
-//        startActivity(i);
-//    }
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        initaliseUI();
-//    }
-//
-//    private void initaliseUI() {
-//        findViewById(R.id.ibtnAnzac).setOnClickListener(anzacListener);
-//        findViewById(R.id.ibtnBolognese).setOnClickListener(bologneseListener);
-//        findViewById(R.id.ibtnCroissant).setOnClickListener(croissantListener);
-//        findViewById(R.id.ibtnPizza).setOnClickListener(pizzaListener);
-//    }
-//}
-
-//*
 
 import android.content.Intent;
+import android.content.res.TypedArray;
+import android.support.annotation.StyleableRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Click Listeners for each button.
+     * The char they pass is a key to tell which items viewFood needs to send.
+     */
+    private final View.OnClickListener anzacListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            viewFood('a');
+        }
+    };
+    private final View.OnClickListener bologneseListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            viewFood('b');
+        }
+    };
+    private final View.OnClickListener croissantListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            viewFood('c');
+        }
+    };
+    private final View.OnClickListener pizzaListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            viewFood('p');
+        }
+    };
+
+    /**
+     * The four images being displayed
+     */
+    private Image imageAnzac;
+    private Image imageBolognese;
+    private Image imageCroissaunt;
+    private Image imagePizza;
+
+    /**
+     * Activity for launching the metadata food view activity.
+     * Switch statements are more fun to than nested if statements. (Subjective)
+     * @param item a char key used to identify which food to examine.
+     */
+    private void viewFood(char item) {
+        Intent i = new Intent();
+        i.setClass(getApplicationContext(), ViewActivity.class);
+        //This is an alphanumeric to correspond to the item being updated.
+        int request_code;
+        switch (item){
+            case 'a':
+                i.putExtra("image", imageAnzac);
+                request_code = 1;
+                break;
+            case 'b':
+                i.putExtra("image", imageBolognese);
+                request_code = 2;
+                break;
+            case 'c':
+                i.putExtra("image", imageCroissaunt);
+                request_code = 3;
+                break;
+            case 'p':
+                i.putExtra("image", imagePizza);
+                request_code = 16;
+                break;
+            default:
+                //Stops the launch of the next activity if switch fails.
+                Log.e("Activity Launch", "Could not launch subsequent activity - Switch defaulted");
+                return;
+        }
+        startActivityForResult(i,request_code);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initaliseUI();
     }
 
-    //TO DO #1 set up intent to get a result
-    public void buttonHandler(View v) {
-        Intent i = new Intent();
-        i.setClass(getApplicationContext(), com.example.a9726446.a5_2p.ViewActivity.class);
-        startActivityForResult(i, 1);
+    private void initaliseUI() {
+        findViewById(R.id.ibtnAnzac).setOnClickListener(anzacListener);
+        findViewById(R.id.ibtnBolognese).setOnClickListener(bologneseListener);
+        findViewById(R.id.ibtnCroissant).setOnClickListener(croissantListener);
+        findViewById(R.id.ibtnPizza).setOnClickListener(pizzaListener);
+
+
+        //This section loads each image's metadata from file.
+        //  id, name, src, key, date, email, rating, shared
+        @StyleableRes //Not sure why I need to do this way but eh.
+        int i = 0;
+        TypedArray typedArray = getResources().obtainTypedArray(R.array.anzac);
+        imageAnzac = new Image(
+                R.drawable.anzac_320px,
+                typedArray.getString(i),
+                typedArray.getString(i+=1),
+                typedArray.getString(i+=1),
+                typedArray.getString(i+=1),
+                typedArray.getString(i+=1),
+                typedArray.getString(i+=1),
+                typedArray.getBoolean(i, false)
+        );
+        findViewById(R.id.ibtnAnzac).setContentDescription(imageAnzac.getName());
+        ((TextView) findViewById(R.id.tvLblAnzac)).setText(imageAnzac.getName());
+        ((TextView) findViewById(R.id.tvDteAnzac)).setText(imageAnzac.getDate());
+
+        i = 0;
+        typedArray = getResources().obtainTypedArray(R.array.bolognese);
+        imageBolognese = new Image(
+                R.drawable.anzac_320px,
+                typedArray.getString(i),
+                typedArray.getString(i+=1),
+                typedArray.getString(i+=1),
+                typedArray.getString(i+=1),
+                typedArray.getString(i+=1),
+                typedArray.getString(i+=1),
+                typedArray.getBoolean(i, false)
+        );
+        findViewById(R.id.ibtnBolognese).setContentDescription(imageBolognese.getName());
+        ((TextView) findViewById(R.id.tvLblBolognese)).setText(imageBolognese.getName());
+        ((TextView) findViewById(R.id.tvDteBolognese)).setText(imageBolognese.getDate());
+
+        i = 0;
+        typedArray = getResources().obtainTypedArray(R.array.croissant);
+        imageCroissaunt = new Image(
+                R.drawable.anzac_320px,
+                typedArray.getString(i),
+                typedArray.getString(i+=1),
+                typedArray.getString(i+=1),
+                typedArray.getString(i+=1),
+                typedArray.getString(i+=1),
+                typedArray.getString(i+=1),
+                typedArray.getBoolean(i, false)
+        );
+        findViewById(R.id.ibtnCroissant).setContentDescription(imageCroissaunt.getName());
+        ((TextView) findViewById(R.id.tvLblCroissant)).setText(imageCroissaunt.getName());
+        ((TextView) findViewById(R.id.tvDteCroissant)).setText(imageCroissaunt.getDate());
+
+        i = 0;
+        typedArray = getResources().obtainTypedArray(R.array.pizza);
+        imagePizza = new Image(
+                R.drawable.anzac_320px,
+                typedArray.getString(i),
+                typedArray.getString(i+=1),
+                typedArray.getString(i+=1),
+                typedArray.getString(i+=1),
+                typedArray.getString(i+=1),
+                typedArray.getString(i+=1),
+                typedArray.getBoolean(i, false)
+        );
+        findViewById(R.id.ibtnPizza).setContentDescription(imagePizza.getName());
+        ((TextView) findViewById(R.id.tvLblPizza)).setText(imagePizza.getName());
+        ((TextView) findViewById(R.id.tvDtePizza)).setText(imagePizza.getDate());
+        typedArray.recycle();
     }
 
-    // TO DO #4 get result back and process/update UI
+    /**
+     * Handles updates to the display.
+     * @param requestCode It's a cheesy way to do it, but switch statements are so much fun~
+     * @param resultCode Determines if the child activity terminated properly
+     * @param intent an intent with data to pass, hopefully!
+     */
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        // TO DO #4a check for result code
-        if (resultCode == RESULT_OK && requestCode == 1){
-            // TO DO #4b if result ok then get data and update UI
-            Image i = intent.getParcelableExtra("List");
-            ((TextView) findViewById(R.id.task_view)).setText(i.toString());
-        } else {
-            // TO DO #4c add elses/Logs for result not okay
-            Log.e("Parcel Failed",String.format("Result Code: %d, Request Code: %d", resultCode, requestCode));
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
+        //Get Errors out of the way first.
+        if (resultCode != RESULT_OK) Log.e("Parcel Failed", String.format(
+                "Result Code %d, Request Code: %d", resultCode, requestCode
+        )); else {
+            Image image = intent.getParcelableExtra("image");
+            switch (requestCode){
+                case 1: //anzac
+                    ((TextView) findViewById(R.id.tvLblAnzac)).setText(image.getName());
+                    ((TextView) findViewById(R.id.tvDteAnzac)).setText(image.getDate());
+                    ((ImageButton) findViewById(R.id.ibtnAnzac)).setContentDescription(image.getName());
+                    break;
+                case 2: //bolognese
+                    ((TextView) findViewById(R.id.tvLblBolognese)).setText(image.getName());
+                    ((TextView) findViewById(R.id.tvDteBolognese)).setText(image.getDate());
+                    break;
+                case 3: //croissant
+                    ((TextView) findViewById(R.id.tvLblCroissant)).setText(image.getName());
+                    ((TextView) findViewById(R.id.tvDteCroissant)).setText(image.getDate());
+                    break;
+                case 16: //pizza
+                    ((TextView) findViewById(R.id.tvLblPizza)).setText(image.getName());
+                    ((TextView) findViewById(R.id.tvDtePizza)).setText(image.getDate());
+                    break;
+                default:
+                    //Stops the launch of the next activity if switch fails.
+                    Log.e("Activity Launch", "Could not launch subsequent activity - Switch defaulted");
+                    //return; //Last statement in void - not needed.
+            }
         }
     }
 }
 
-// */
+//import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//import android.util.Log;
+//import android.view.View;
+//import android.widget.TextView;
+//
+//import java.util.ArrayList;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//
+//    //TO DO #1 set up intent to get a result
+//    public void buttonHandler(View v) {
+//        Intent i = new Intent();
+//        i.setClass(getApplicationContext(), com.example.a9726446.a5_2p.ViewActivity.class);
+//        startActivityForResult(i, 1);
+//    }
+//
+//    // TO DO #4 get result back and process/update UI
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+//        // TO DO #4a check for result code
+//        if (resultCode == RESULT_OK && requestCode == 1){
+//            // TO DO #4b if result ok then get data and update UI
+//            Image i = intent.getParcelableExtra("List");
+//            ((TextView) findViewById(R.id.task_view)).setText(i.getName());
+//        } else {
+//            // TO DO #4c add elses/Logs for result not okay
+//            Log.e("Parcel Failed",String.format(
+//                  "Result Code: %d, Request Code: %d", resultCode, requestCode
+//            ));
+//        }
+//    }
+//}
--- app/src/main/java/com/example/a9726446/a5_2p/ViewActivity.java
+++ app/src/main/java/com/example/a9726446/a5_2p/ViewActivity.java
@@ -1,142 +1,183 @@
 package com.example.a9726446.a5_2p;
-//
-//import android.app.DatePickerDialog;
-//import android.os.Bundle;
-//import android.support.v7.app.AppCompatActivity;
-//import android.util.Log;
-//import android.view.View;
-//import android.widget.DatePicker;
-//import android.widget.EditText;
-//import android.widget.ImageView;
-//import android.widget.TextView;
-//
-//import java.text.SimpleDateFormat;
-//import java.util.Calendar;
-//import java.util.Locale;
-//
-///**
-// * Created by 9726446 on 21/09/2018 at EN302-018
-// */
-//
-//public class ViewActivity extends AppCompatActivity {
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState){
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_view);
-//        initaliseUI();
-//    }
-//
-//    private final Calendar aCalendar = Calendar.getInstance();
-//
-//    private void initaliseUI() {
-//        try {
-//            startCalendar();
-//            Bundle b = getIntent().getExtras().getBundle("bundle");
-//            if (b == null) return;
-//            ((ImageView)(findViewById(R.id.ivBigFood))).setImageResource(b.getInt("imageID"));
-//            findViewById(R.id.ivBigFood).setContentDescription(getString(b.getInt("captionID")));
-//            ((TextView)(findViewById(R.id.etTitle))).setText(b.getInt("captionID"));
-//            ((TextView)(findViewById(R.id.etSrc))).setText(b.getInt("srcID"));
-//
-//        } catch (NullPointerException npe) {
-//            Log.e("Bundle Error (View)", npe.toString());
-//        }
-//    }
-//
-//    /**
-//     * With guidance from https://stackoverflow.com/questions/14933330/datepicker-how-to-popup-datepicker-when-click-on-edittext#
-//     */
-//    private void startCalendar() {
-//        final EditText editText = (EditText) findViewById(R.id.etDate);
-//        final DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
-//            @Override
-//            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-//                aCalendar.set(Calendar.YEAR, year);
-//                aCalendar.set(Calendar.MONTH, month);
-//                aCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-//                String formatStr = "dd/MM/yy"; //Calendar Format
-//                SimpleDateFormat sdf = new SimpleDateFormat(formatStr, Locale.ENGLISH);
-//                editText.setText(sdf.format(aCalendar.getTime()));
-//            }
-//        };
-//
-//        editText.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                new DatePickerDialog(ViewActivity.this, dateSetListener,
-//                        aCalendar.get(Calendar.YEAR),
-//                        aCalendar.get(Calendar.MONTH),
-//                        aCalendar.get(Calendar.DAY_OF_MONTH)
-//                ).show();
-//            }
-//        });
-//
-//    }
-//
-//}
 
-//*
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.ImageView;
+import android.widget.ToggleButton;
 
-public class ViewActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-    private String grade;
-    private boolean complete = false;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+/**
+ * Created by 9726446 on 21/09/2018 at EN302-018
+ */
+
+public class ViewActivity extends AppCompatActivity {
+    //Holder local vars, required for various reasons.
+    int imageId;
+    boolean isShared = false;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
-
-        Spinner spinner = findViewById(R.id.grade_spinner);
-        spinner.setOnItemSelectedListener(this);
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        grade = parent.getItemAtPosition(position).toString();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-        grade = parent.getItemAtPosition(0).toString();
+        initaliseUI();
     }
 
     public void onCheckboxClicked(View view) {
-        complete = ((CheckBox) view).isChecked();
+        isShared = ((CheckBox) view).isChecked();
     }
 
-    // TO DO #5 add in some simple form validation
-
-    /**
-     * Referring:
-     * https://stackoverflow.com/questions/10407159/how-to-manage-startactivityforresult-on-android
-     * */
-    @Override
     public void onBackPressed() {
-        // TO DO #3 handle returning the form details when back pressed
-        // TO DO #3a create intent and task
         Intent i = new Intent();
-        // TO DO #3b create list for task to be attached to parcelable
+        //  id, name, src, key, date, email, rating, shared
         Image image = new Image(
-                ((EditText)findViewById(R.id.task_name)).getText().toString(),
-                grade,
-                complete
-        );
-
-        // TO DO #3c add list to intent, set result
-        i.putExtra("List",image);
+                imageId,
+                ((EditText)findViewById(R.id.etName)).getText().toString(),
+                ((EditText)findViewById(R.id.etSrc)).getText().toString(),
+                ((EditText)findViewById(R.id.etKey)).getText().toString(),
+                ((EditText)findViewById(R.id.etDate)).getText().toString(),
+                ((EditText)findViewById(R.id.etEmail)).getText().toString(),
+                ((EditText)findViewById(R.id.etRating)).getText().toString(),
+                isShared);
+        i.putExtra("image", image);
         setResult(Activity.RESULT_OK, i);
-        // TO DO #3d return
         finish();
     }
 
+    private final Calendar aCalendar = Calendar.getInstance();
+
+    private void initaliseUI() {
+        try {
+            startCalendar();
+            Image image = getIntent().getExtras().getParcelable("image");
+            if (image == null){
+                Log.e("metadata init","Couldn't find the Parcelled Image!");
+                setResult(Activity.RESULT_CANCELED);
+                finish();
+                return;
+            }
+            ((ImageView) findViewById(R.id.ivFood)).setImageResource(image.getResourceID());
+            imageId = image.getResourceID(); //because android is silly and can't get it after the fact. :/
+            ((EditText) findViewById(R.id.etName)).setText(image.getName());
+            ((EditText) findViewById(R.id.etSrc)).setText(image.getSrc());
+            ((EditText) findViewById(R.id.etKey)).setText(image.getKey());
+            ((EditText) findViewById(R.id.etDate)).setText(image.getDate());
+            ((ToggleButton) findViewById(R.id.tbShare)).setChecked(image.isShared());
+            ((EditText) (findViewById(R.id.etEmail))).setText(image.getEmail());
+            ((EditText) findViewById(R.id.etRating)).setText(image.getRating());
+
+        } catch (NullPointerException npe) {
+            Log.e("Bundle Error (View)", npe.toString());
+            setResult(Activity.RESULT_CANCELED);
+            finish();
+        }
+    }
+
+    /**
+     * With guidance from https://stackoverflow.com/questions/14933330/datepicker-how-to-popup-datepicker-when-click-on-edittext#
+     */
+    private void startCalendar() {
+        final EditText editText = (EditText) findViewById(R.id.etDate);
+        final DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                aCalendar.set(Calendar.YEAR, year);
+                aCalendar.set(Calendar.MONTH, month);
+                aCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                String formatStr = "dd/MM/yy"; //Calendar Format
+                SimpleDateFormat sdf = new SimpleDateFormat(formatStr, Locale.ENGLISH);
+                editText.setText(sdf.format(aCalendar.getTime()));
+            }
+        };
+
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(ViewActivity.this, dateSetListener,
+                        aCalendar.get(Calendar.YEAR),
+                        aCalendar.get(Calendar.MONTH),
+                        aCalendar.get(Calendar.DAY_OF_MONTH)
+                ).show();
+            }
+        });
+
+    }
 
 }
+
+//*
+//import android.app.Activity;
+//import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.AdapterView;
+//import android.widget.CheckBox;
+//import android.widget.EditText;
+//import android.widget.Spinner;
+//
+//public class ViewActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+//    private String grade;
+//    private boolean complete = false;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_view);
+//
+//        //Spinner spinner = findViewById(R.id.grade_spinner);
+//        //spinner.setOnItemSelectedListener(this);
+//    }
+//
+//    @Override
+//    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//        grade = parent.getItemAtPosition(position).getName();
+//    }
+//
+//    @Override
+//    public void onNothingSelected(AdapterView<?> parent) {
+//        grade = parent.getItemAtPosition(0).getName();
+//    }
+//
+//    public void onCheckboxClicked(View view) {
+//        complete = ((CheckBox) view).isChecked();
+//    }
+//
+//    // TO DO #5 add in some simple form validation
+//
+//    /**
+//     * Referring:
+//     * https://stackoverflow.com/questions/10407159/how-to-manage-startactivityforresult-on-android
+//     * */
+//    @Override
+//    public void onBackPressed() {
+//        // TO DO #3 handle returning the form details when back pressed
+//        // TO DO #3a create intent and task
+//        Intent i = new Intent();
+//        int image = 0;
+//        // TO DO #3b create list for task to be attached to parcelable
+////        Image image = new Image(
+////                ((EditText)findViewById(R.id.task_name)).getText().getName(),
+////                grade,
+////                complete
+////        );
+//
+//        // TO DO #3c add list to intent, set result
+//        i.putExtra("List",image);
+//        setResult(Activity.RESULT_OK, i);
+//        // TO DO #3d return
+//        finish();
+//    }
+//
+//
+//}
